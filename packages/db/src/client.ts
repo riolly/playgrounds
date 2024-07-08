b98f2b9f55e1fdec7a02f37d8391bd007c1efb00
riolly <alberto.cahyantara@gmail.com>
2024-07-08 22:02:28 +0900

    use neon db

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,6 +1,14 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
+
+// eslint-disable-next-line no-restricted-properties
+const postgresUrl = process.env.POSTGRES_URL;
+if (!postgresUrl) {
+  throw new Error("POSTGRES_URL is not defined");
+}
+
+const sql = neon(postgresUrl);
 
 export const db = drizzle(sql, { schema });
