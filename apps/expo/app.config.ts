bfec4ee67476c7224cf47e17d8786c111a3abaaa
riolly <alberto.cahyantara@gmail.com>
2024-07-15 15:51:03 +0900

    fix: build node_env

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -26,7 +26,7 @@ const vars = {
 
 function getEnv(variable: keyof typeof vars) {
   const variant =
-    (process.env.NODE_ENV as "development" | "preview" | "production") ??
+    (process.env.BUILD_TARGET as "development" | "preview" | "production") ??
     "development";
   return vars[variable][variant];
 }
